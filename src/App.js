c0b0d28ca5c2479a054847a1fe9267e15a8593f6
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-09 12:20:46 +0530

    student app front page completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import FontPage from './components/FontPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <FontPage/> 
+      
     </div>
   );
 }
---
+++ src/components/FontPage.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+
+const FontPage = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div id="carouselExampleIndicators" class="carousel slide">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="https://fisat.ac.in/wp-content/uploads/2023/09/Enquiry-scaled.jpg" class="d-block w-100" alt="..."></img>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://fpimages.withfloats.com/actual/5892d833abd1350a1c13c152.jpg" class="d-block w-100" alt="..."></img>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://fisat.ac.in/wp-content/uploads/2024/05/Placement-Admission-Banner-scaled.jpg" class="d-block w-100" alt="..."></img>
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div><br />
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h1 class="card-title"><center>21 Years</center></h1><br />
+                                        <h5 class="card-text bg-primary"><center>Legacy</center> <br /><br /><br /></h5>
+                                        <br />
+
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h1 class="card-title"><center>Rs.12 cr+</center></h1><br />
+                                        <h5 class="card-text bg-primary"><center>Worth of Scholar</center> <br /><br /><br /></h5>
+                                        <br />
+
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h1 class="card-title"><center>736 * offers</center></h1><br />
+                                        <h5 class="card-text bg-primary"><center>Class of 2023</center> <br /><br /><br /></h5>
+                                        <br />
+
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h1 class="card-title"><center>2500+</center></h1><br />
+                                        <h5 class="card-text bg-primary"><center>students</center> <br /><br /><br /></h5>
+                                        <br />
+
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div><br />
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h1><center>Explore</center></h1>
+                                        <h5><center>Have a look at what Fisat has to offer</center></h5>
+                                    </div>
+                                </div>
+                            </div><br />
+                        </div>
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-3 col-lg-4 col-xl-4 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://fisat.ac.in/wp-content/uploads/2022/06/sports-scaled.jpg" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h1 class="card-title">Campus life</h1>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-3 col-lg-4 col-xl-4 col-xxl-3">
+                                <div class="card" >
+                                    <div class="card-body">
+                                        <h1 class="card-title">Facilities</h1>
+
+                                    </div>
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6HYbKQAx1fYFY5iHSzq-PjHEslI62bbPRUQ&s" class="card-img-top" alt="..."></img>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-3 col-lg-4 col-xl-4 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://fisat.ac.in/wp-content/uploads/2022/07/library-scaled.jpg" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h1 class="card-title">Library</h1>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-3 col-lg-4 col-xl-4 col-xxl-3">
+                            <div class="card" >
+                                    <div class="card-body">
+                                        <h1 class="card-title">Interaction</h1>
+                                    </div>
+                                    <img src="https://fisat.ac.in/wp-content/uploads/2022/07/industry.jpg" class="card-img-top" alt="..."></img>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FontPage
